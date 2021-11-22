289e99893fac61cb13eed306f470ef004e8033c4
Erel Segal-Halevi <erelsgl@gmail.com>
2021-11-22 15:36:48 +0200

    best coin strategy

--- examples/best_coin_picking_strategy.py
+++ examples/best_coin_picking_strategy.py
@@ -52,35 +52,6 @@ def max_value(coins: List[Number], first:bool):
     value = dynprog.max_value(initial_states=initial_states, neighbors=neighbors, final_states=final_states)
     return value if first else -value
 
-# def max_value_solution(string:str,count_states=False):
-#     """
-#     Finds the longest palyndromic subsequence in the given string.
-
-#     >>> LPS_string("a")
-#     'a'
-#     >>> LPS_string("bb")
-#     'bb'
-#     >>> LPS_string("abcdba")
-#     'abcba'
-#     >>> LPS_string("programming")
-#     'gmmg'
-#     """
-#     lenstr = len(string)
-#     def initial_states():  # returns (state,value,data) tuples
-#         for i in range(lenstr): yield ((i,i+1),1,string[i])
-#         for i in range(lenstr): yield ((i,i),0,"")
-#     def neighbors (state:Tuple[int,int], value:int, data:str): # returns (state,value,data) tuples
-#         (i,j) = state    # indices: represent the string between i and j-1.
-#         if i>0 and j<lenstr and string[i-1]==string[j]:  # add two letters to the palyndrome
-#             yield ((i-1,j+1),value+2,string[i-1]+data+string[j])
-#         else:    # add one letter in each side of the string, without extending the palyndrome
-#             if i>0:  yield ((i-1,j),value,data)
-#             if j<lenstr:  yield ((i,j+1),value,data)
-#     def final_states():
-#         yield (0,lenstr)
-#     (state, value, data, num_of_states) = dynprog.max_value_solution(initial_states=initial_states, neighbors=neighbors, final_states=final_states)
-#     return (state,value,data,num_of_states) if count_states else data
-
 
 if __name__=="__main__":
     import sys, logging
