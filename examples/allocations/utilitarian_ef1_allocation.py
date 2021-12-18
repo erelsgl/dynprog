4b8a2a9d4f251919a4994041b1acd963138775a7
Erel Segal-Halevi <erelsgl@gmail.com>
2021-12-18 21:09:31 +0200

    Move all to sequential

--- examples/allocations/utilitarian_ef1_allocation.py
+++ examples/allocations/utilitarian_ef1_allocation.py
@@ -1,21 +1,24 @@
 #!python3
 
 """
-Uses the generic dynamic programming function to find an EF1/EFx allocation
+Uses the sequential dynamic programming function to find an EF1/EFx allocation
 of items to agents with different valuations, with a largest sum of utilities (utilitarian value).
 
 The input is a valuation-matrix v, where v[i][j] is the value of agent i to item j.
 
-The states are of the form  (j, (v1, v2, ..., vn)) where n is the number of agents.
-The "j" is the number of items allocated so far.
-The "vi" are the value of bundle i to agent i.
+The states are of the form (d11, d12, ..., dnn; b11, b12, ..., bnn) where n is the number of agents.
+where dij := vi(Ai)-vi(Aj).
+and   bij is the largest value for i of an item allocated to j.
 
 Programmer: Erel Segal-Halevi
-Since: 2021-11
+Since: 2021-12
 """
 
-import dynprog, math, logging, itertools
+import dynprog, math, logging
 from typing import *
+from dynprog.sequential import SequentialDynamicProgram
+
+from common import add_input_to_agent_value, add_input_to_bin, items_as_value_vectors
 
 logger = logging.getLogger(__name__)
 
@@ -46,6 +49,9 @@ def utilitarian_ef1_value(valuation_matrix, efx=False):
     >>> utilitarian_ef1_value([[98,91,29,50,76,94],[43,67,93,35,49,12],[45,10,62,47,82,60]],efx=True)
     481.0
     """
+    items = items_as_value_vectors(valuation_matrix)
+    return PartitionDP(valuation_matrix, efx).max_value(items)
+
     num_of_agents   = len(valuation_matrix)
     num_of_items    = len(valuation_matrix[0])
     def is_ef1(bundle_differences:List[List], largest_value_owned_by_others:List[List])->bool:
@@ -97,10 +103,106 @@ def utilitarian_ef1_value(valuation_matrix, efx=False):
 
 
 
+
+#### Dynamic program definition:
+
+class PartitionDP(SequentialDynamicProgram):
+
+    # The states are of the form (d11, d12, ..., dnn; b11, b12, ..., bnn) where n is the number of agents.
+    # where dij := vi(Ai)-vi(Aj).
+    # and   bij is the largest value for i of an item allocated to j.
+
+    def __init__(self, valuation_matrix, efx=False):
+        num_of_agents = self.num_of_agents = len(valuation_matrix)
+        self.thresholds = [sum(valuation_matrix[i])/num_of_agents for i in range(num_of_agents)]
+        self.valuation_matrix = valuation_matrix
+        self.sum_valuation_matrix =  sum(map(sum,valuation_matrix))
+        self.efx = efx
+
+    def initial_states(self):
+        zero_differences = self.num_of_agents * (self.num_of_agents*(0,),)
+        # print("zero_differences",zero_differences)
+        initial_value_to_remove = math.inf if self.efx else 0
+        largest_value_owned_by_others = self.num_of_agents * (self.num_of_agents*(initial_value_to_remove,),)
+        return {(zero_differences, largest_value_owned_by_others)}
+
+    def initial_solution(self):
+        empty_bundles = [ [] for _ in range(self.num_of_agents)]
+        return empty_bundles
+   
+    def transition_functions(self):
+        return [
+            lambda state, input, agent_index=agent_index: (\
+                _update_bundle_differences(state[0], agent_index, input), \
+                _update_value_owned_by_others(state[1], agent_index, input[-1], self.valuation_matrix, self.efx) 
+            )
+            for agent_index in range(self.num_of_agents)
+        ]
+
+    def construction_functions(self):
+        return [
+            lambda solution,input,agent_index=agent_index: add_input_to_bin(solution, agent_index, input[-1])
+            for agent_index in range(self.num_of_agents)
+        ]
+
+    def value_function(self):
+        return lambda state: (sum(map(sum,state[0]))+self.sum_valuation_matrix) / self.num_of_agents if self._is_ef1(state[0],state[1]) else -math.inf
+    
+    def _is_ef1(self, bundle_differences:list, largest_value_owned_by_others:list)->bool:
+        return all([bundle_differences[i][j] + largest_value_owned_by_others[i][j]>=0 
+            for i in range(self.num_of_agents) for j in range(self.num_of_agents)])
+
+
+
+def _update_bundle_differences(bundle_differences, agent_index, item_values):
+    """
+    >>> _update_bundle_differences( ((0,0),(0,0)), 0, [11,33,0]  )
+    ((0, 11), (-33, 0))
+    >>> _update_bundle_differences( ((0,0),(0,0)), 1, [11,33,0]  )
+    ((0, -11), (33, 0))
+    """
+    # print("bundle_differences",bundle_differences)
+    num_of_agents = len(bundle_differences)
+    new_bundle_differences = [list(d) for d in bundle_differences]
+    for other_agent_index in range(num_of_agents):
+        if other_agent_index==agent_index: continue
+        new_bundle_differences[agent_index][other_agent_index] += item_values[agent_index]
+        new_bundle_differences[other_agent_index][agent_index] -= item_values[other_agent_index]
+    new_bundle_differences = tuple((tuple(d) for d in new_bundle_differences))
+    return new_bundle_differences
+
+def _update_value_owned_by_others(largest_value_owned_by_others:list, agent_index:int, item_index:int, valuation_matrix, efx=False):
+    """
+    Update the matrix of largest-value-owned-by-others when 
+    the item #item_index is given to the agent #agent_index.
+    >>> _update_value_owned_by_others([[0,0,0],[0,0,0],[0,0,0]], 0, 0, [[55,66,77],[88,99,11],[22,33,44]])
+    ((0, 0, 0), (88, 0, 0), (22, 0, 0))
+    >>> _update_value_owned_by_others([[0,20,30],[40,0,60],[70,80,0]], 0, 0, [[55,66,77],[88,99,11],[22,33,44]])
+    ((0, 20, 30), (88, 0, 60), (70, 80, 0))
+    """
+    num_of_agents = len(largest_value_owned_by_others)
+    new_largest_value_owned_by_others = [list(d) for d in largest_value_owned_by_others]
+    for other_agent_index in range(num_of_agents):
+        if other_agent_index==agent_index: continue
+        other_agent_value = valuation_matrix[other_agent_index][item_index]
+        if efx:
+            replace_item = other_agent_value < new_largest_value_owned_by_others[other_agent_index][agent_index]
+        else: # ef1
+            replace_item = other_agent_value > new_largest_value_owned_by_others[other_agent_index][agent_index]
+        if replace_item:
+            new_largest_value_owned_by_others[other_agent_index][agent_index] = other_agent_value
+    new_largest_value_owned_by_others = tuple((tuple(d) for d in new_largest_value_owned_by_others))
+    return new_largest_value_owned_by_others
+
+
+
+
+
+
 if __name__=="__main__":
     import sys
-    dynprog.general.logger.addHandler(logging.StreamHandler(sys.stdout))
-    dynprog.general.logger.setLevel(logging.WARNING)
+    dynprog.sequential.logger.addHandler(logging.StreamHandler(sys.stdout))
+    dynprog.sequential.logger.setLevel(logging.WARNING)
     logger.addHandler(logging.StreamHandler(sys.stdout))
     logger.setLevel(logging.WARNING)
 
@@ -108,10 +210,9 @@ if __name__=="__main__":
     (failures,tests) = doctest.testmod(report=True)
     print ("{} failures, {} tests".format(failures,tests))
 
-    dynprog.general.logger.setLevel(logging.WARNING)
+    dynprog.sequential.logger.setLevel(logging.WARNING)
     import numpy as np
-    valuation_matrix = np.random.randint(0,99, [3,6])   # ~ 1093 states
-    # valuation_matrix = np.random.randint(0,99, [3,7])   # ~ 3280 states
+    valuation_matrix = np.random.randint(0,9, [3,10])   # ~ 80k states
     print("valuation_matrix:\n",valuation_matrix)
-    print("EF1: ",utilitarian_ef1_value(valuation_matrix))
-    print("EFx: ",utilitarian_ef1_value(valuation_matrix,efx=True))
+    print("Utilitarian within EF1: ",utilitarian_ef1_value(valuation_matrix))
+    print("Utilitarian within EFx: ",utilitarian_ef1_value(valuation_matrix,efx=True))
--- examples/allocations/utilitarian_envyfree_allocation.py
+++ examples/allocations/utilitarian_envyfree_allocation.py
@@ -44,38 +44,6 @@ def utilitarian_envyfree_value(valuation_matrix):
     items = items_as_value_vectors(valuation_matrix)
     return PartitionDP(valuation_matrix).max_value(items)
 
-    # Algorithm: add the items one by one to all possible bundles.
-    num_of_agents   = len(valuation_matrix)
-    num_of_items    = len(valuation_matrix[0])
-    def is_envyfree(bundle_differences:list)->bool:
-        return all([bundle_differences[i][j]>=0 for i in range(num_of_agents) for j in range(num_of_agents)])
-    def initial_states():  # returns (state,value) tuples
-        zero_differences = num_of_agents * (num_of_agents*(0,),)
-        yield ( (0, zero_differences), -math.inf)
-    def neighbors (state:Tuple[int,tuple], value:int):   # returns (state,value) tuples
-        (item_index, bundle_differences) = state
-        if item_index < num_of_items:
-            next_item_index = item_index+1
-            for agent_index in range(num_of_agents):
-                # Update bundle differences
-                new_bundle_differences = [list(d) for d in bundle_differences]
-                for other_agent_index in range(num_of_agents):
-                    if other_agent_index==agent_index: continue
-                    new_bundle_differences[agent_index][other_agent_index] += valuation_matrix[agent_index][item_index]
-                    new_bundle_differences[other_agent_index][agent_index] -= valuation_matrix[other_agent_index][item_index]
-                new_bundle_differences = tuple((tuple(d) for d in new_bundle_differences))
-
-                # Check envy-freeness:
-                state_value = -math.inf 
-                if next_item_index == num_of_items and is_envyfree(new_bundle_differences): 
-                    state_value = sum(map(sum,new_bundle_differences))
-                yield ((next_item_index, new_bundle_differences), state_value)
-    def is_final_state(state):
-        (item_index, _) = state
-        return item_index==num_of_items
-    value = dynprog.general.max_value(initial_states=initial_states, neighbors=neighbors, is_final_state=is_final_state)
-    return (value + sum(map(sum,valuation_matrix))) / num_of_agents
-
 
 
 #### Dynamic program definition:
